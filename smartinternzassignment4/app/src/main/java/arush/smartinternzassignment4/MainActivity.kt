c977aab32bb32dedf58790691e788bf7d421a054
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-09-17 11:59:29 +0530

    home page created

--- smartinternzassignment4/app/src/main/java/arush/smartinternzassignment4/MainActivity.kt
+++ smartinternzassignment4/app/src/main/java/arush/smartinternzassignment4/MainActivity.kt
@@ -1,19 +1,21 @@
 package arush.smartinternzassignment4
 
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,8 +25,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -34,7 +36,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -112,7 +113,6 @@ fun HomeScreen(logOut : () -> Unit) {
         var restList = arrayListOf<String>("Cafe coffee day","Domino's pizza","KFC","Burger king","Biryani house"
             ,"Royal cuisine")
         var descList = arrayListOf<String>("ccd","dominos","kfc","burger king","biryani","royal restaurant")
-        var aboutList = arrayListOf<String>()
         var cuisineList = arrayListOf<String>("North Indian", "South Indian")
         Column (modifier = Modifier
             .fillMaxSize()
@@ -121,16 +121,36 @@ fun HomeScreen(logOut : () -> Unit) {
             Column (horizontalAlignment = Alignment.CenterHorizontally){
                 Row(horizontalArrangement = Arrangement.SpaceAround, verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier.padding(top = 5.dp)) {
-                    Divider(color = Color(0xFF8B8B8B), thickness = 1.dp, modifier = Modifier.weight(1f))
-                    Text(text = "RECOMMENDED FOR YOU", color = Color(0xFF8B8B8B), style = TextStyle(fontSize = 18.sp))
-                    Divider(color = Color(0xFF8B8B8B), thickness = 1.dp, modifier = Modifier.weight(1f))
+                    Divider(color = Color(0xFFD5D5D5), thickness = 1.dp, modifier = Modifier.weight(1f))
+                    Text(text = "  RECOMMENDED FOR YOU  ", color = Color(0xFF8F8F8F), style = TextStyle(fontSize = 18.sp))
+                    Divider(color = Color(0xFFD5D5D5), thickness = 1.dp, modifier = Modifier.weight(1f))
                 }
-                recommendation(bgImage = imageList, bgDesc = descList, restName = restList,
-                    aboutList = aboutList, cuisineList = cuisineList)
+
+                var ranOne = Random.nextInt(0..imageList.size-1)
+                var ranTwo = Random.nextInt(0..imageList.size-1)
+                var ranCuisine = Random.nextInt(0..1)
+                var ranCuisineTwo = if(ranCuisine==1) 0 else 1
+                while (ranOne == ranTwo){ranTwo = Random.nextInt(0..imageList.size-1)}
+
+                recommendation(bgImage = imageList[ranOne], bgDesc = descList[ranOne], restName = restList[ranOne],
+                    cuisineList = cuisineList[ranCuisine])
+                recommendation(bgImage = imageList[ranTwo], bgDesc = descList[ranTwo], restName = restList[ranTwo],
+                    cuisineList = cuisineList[ranCuisineTwo])
             }
 
-            hotelCard(bgImage = imageList, bgDesc = descList, restName = restList,
-                aboutList = aboutList, cuisineList = cuisineList)
+            Row(horizontalArrangement = Arrangement.SpaceAround, verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(top = 30.dp)) {
+                Divider(color = Color(0xFFD5D5D5), thickness = 1.dp, modifier = Modifier.weight(1f))
+                Text(text = "  ALL  RESTAURANTS  ", color = Color(0xFF8F8F8F), style = TextStyle(fontSize = 18.sp))
+                Divider(color = Color(0xFFD5D5D5), thickness = 1.dp, modifier = Modifier.weight(1f))
+            }
+
+            hotelCard(bgImage = imageList[0], bgDesc = descList[0], restName = restList[0], cuisineList = cuisineList[0], {})
+            hotelCard(bgImage = imageList[1], bgDesc = descList[1], restName = restList[1], cuisineList = cuisineList[1], {})
+            hotelCard(bgImage = imageList[2], bgDesc = descList[2], restName = restList[2], cuisineList = cuisineList[1], {})
+            hotelCard(bgImage = imageList[3], bgDesc = descList[3], restName = restList[3], cuisineList = cuisineList[0], {})
+            hotelCard(bgImage = imageList[4], bgDesc = descList[4], restName = restList[4], cuisineList = cuisineList[1], {})
+            hotelCard(bgImage = imageList[5], bgDesc = descList[5], restName = restList[5], cuisineList = cuisineList[0], {})
         }
     }
 
@@ -144,55 +164,111 @@ fun GreetingPreview() {
     }
 }
 
+private fun rating(): Float {
+    val randomValue = Random.nextFloat() * (5.0 - 3.0) + 3.0
+    return "%.1f".format(randomValue).toFloat()
+}
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun hotelCard(bgImage: ArrayList<Int>, bgDesc: ArrayList<String>, restName: ArrayList<String>,
-              aboutList: ArrayList<String>, cuisineList: ArrayList<String>){
-    var i = 0
-    Card(
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 8.dp
-        ),
-        shape = RoundedCornerShape(8.dp),
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(start = 14.dp, end = 14.dp, top = 18.dp),
-        colors = CardDefaults.cardColors(Color.White)
-    ) {
-    }
+fun hotelCard(bgImage: Int, bgDesc: String, restName: String, cuisineList: String, clickFunction: ()->Unit){
+        Card(
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            ),
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 14.dp, end = 14.dp, top = 18.dp),
+            colors = CardDefaults.cardColors(Color.White),
+            onClick = {clickFunction()}
+        ) {
+            Column {
+                Image(painter = painterResource(id = bgImage), contentDescription = bgDesc,
+                    modifier = Modifier.height(200.dp), contentScale = ContentScale.Crop)
+
+                Column(modifier = Modifier.padding( end = 10.dp, top = 10.dp, bottom = 5.dp)) {
+                    Row (modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 10.dp)){
+                        Text(text = restName, style = TextStyle(fontFamily = FontFamily(Font((R.font.poppins_bold),
+                            weight = FontWeight(1))), fontSize = 24.sp,))
+                        Box(modifier = Modifier
+                            .fillMaxSize()
+                            .align(Alignment.CenterVertically)){
+                            Row (modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.End){
+                                Box(modifier = Modifier
+                                    .clip(shape = RoundedCornerShape(8.dp))
+                                    .background(Color(0xFF2A9E00))
+                                    .size(50.dp, 26.dp)){
+                                    Row {
+                                        Text(text = rating().toString(), style = TextStyle(fontSize = 18.sp),
+                                            color = Color.White, modifier = Modifier.padding(start = 5.dp))
+                                        Image(painter = painterResource(id = R.drawable.baseline_star_24),
+                                            contentDescription = "Star", modifier = Modifier
+                                                .size(28.dp)
+                                                .padding(start = 1.dp, end = 5.dp)
+                                                .align(Alignment.CenterVertically))
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                        Text(text = cuisineList, style = TextStyle(fontSize = 15.sp,
+                            fontFamily = FontFamily(Font((R.font.poppins_regular), weight = FontWeight(1)))),
+                            color = Color(0xFF9B9B9B), modifier = Modifier.padding(start = 10.dp, end = 8.dp)
+                        )
+
+                        Image(painter = painterResource(id = R.drawable.baseline_circle_24), contentDescription = "circle",
+                            modifier = Modifier.size(14.dp).padding(bottom = 3.dp))
+
+                        Row (modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 5.dp), verticalAlignment = Alignment.CenterVertically){
+                            Image(painter = painterResource(id = R.drawable.timer_pic), contentDescription = "Watch",
+                                modifier = Modifier.size(20.dp).padding(bottom = 3.dp))
+                            Text(text = "25-35 min", style = TextStyle(fontSize = 15.sp,
+                                fontFamily = FontFamily(Font((R.font.poppins_regular), weight = FontWeight(1)))),
+                                color = Color(0xFF9B9B9B))
+                        }
+                    }
+                }
+            }
+        }
 }
 
 @Composable
-fun recommendation(bgImage: ArrayList<Int>, bgDesc: ArrayList<String>, restName: ArrayList<String>,
-                   aboutList: ArrayList<String>, cuisineList: ArrayList<String>){
+fun recommendation(bgImage: Int, bgDesc: String, restName: String, cuisineList: String){
 
-    var ranOne = Random.nextInt(0..bgImage.size-1)
-    var ranTwo = Random.nextInt(0..bgImage.size-1)
-    var ranCuisine = Random.nextInt(0..1)
-    while (ranOne != ranTwo){ranTwo = Random.nextInt(0..bgImage.size)}
     Card(modifier = Modifier
         .fillMaxWidth(0.92f)
         .padding(top = 14.dp),
         elevation = CardDefaults.cardElevation(10.dp),
         colors = CardDefaults.cardColors(Color.White)) {
         Row {
-            Image(painter = painterResource(id = bgImage[ranOne]),
-                contentDescription = bgDesc[ranOne], contentScale = ContentScale.Crop,
+            Image(painter = painterResource(id = bgImage),
+                contentDescription = bgDesc, contentScale = ContentScale.Crop,
                 modifier = Modifier.size(70.dp))
             Column(modifier = Modifier.padding(start = 10.dp, top = 5.dp)) {
-                Text(text = restName[ranOne], style = TextStyle(fontSize = 16.sp), color = Color.Black)
-                Text(text = cuisineList[ranCuisine], style = TextStyle(fontSize = 13.sp), color = Color(
-                    0xFFA0A0A0
-                )
+                Text(text = restName, style = TextStyle(fontSize = 16.sp), color = Color.Black)
+                Text(text = cuisineList, style = TextStyle(fontSize = 15.sp), color = Color(0xFFA0A0A0)
                 )
             }
-            Row(
-                horizontalArrangement = Arrangement.End,
-                modifier = Modifier
-                    .fillMaxWidth().fillMaxHeight(0.05f).padding(top = 10.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.CenterVertically)) {
-
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.CenterVertically)){
+                Button(onClick = { /*TODO*/ }, modifier = Modifier
+                    .size(78.dp, 36.dp)
+                    .align(Alignment.CenterEnd)
+                    .padding(end = 10.dp)
+                    , colors = ButtonDefaults.buttonColors(Color(0xFFFFB3B9)),
+                    contentPadding = PaddingValues(),
+                    shape = RoundedCornerShape(8.dp),
+                    border = BorderStroke(1.dp, Color(0xFFE93545))
+                ) {
+                    Text(text = "ORDER", color = Color(0xFFE93545), style = TextStyle(fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold))
                 }
             }
         }
