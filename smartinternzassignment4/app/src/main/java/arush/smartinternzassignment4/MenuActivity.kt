0becaf2048a25e56ccb278a78f5b45848ba7c869
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-09-17 23:16:16 +0530

    assignment 4 minor changes

--- smartinternzassignment4/app/src/main/java/arush/smartinternzassignment4/MenuActivity.kt
+++ smartinternzassignment4/app/src/main/java/arush/smartinternzassignment4/MenuActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -46,6 +47,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -121,47 +123,81 @@ fun menuScreen(num: Int) {
             .fillMaxSize()
             .verticalScroll(state = rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally){
-            cardMaker(num) { itemCount++ }
+            val menuId = R.array.Menu0 + num
+            val menuArray = stringArrayResource(id = menuId)
+            var i = 0
+            val map = mutableMapOf<String, ArrayList<Int>>()
+            map["menu0"] = arrayListOf(R.drawable.ccdmenu0,R.drawable.ccdmenu1,R.drawable.ccdmenu2,R.drawable.ccdmenu3,
+                R.drawable.ccdmenu4,R.drawable.ccdmenu5)
+            map["menu1"] = arrayListOf(R.drawable.dominosmenu0,R.drawable.dominosmenu1,R.drawable.dominosmenu2,R.drawable.dominosmenu3,
+                R.drawable.dominosmenu4,R.drawable.dominosmenu5)
+            map["menu2"] = arrayListOf(R.drawable.kfcmenu0,R.drawable.kfcmenu1,R.drawable.kfcmenu2,R.drawable.kfcmenu3,
+                R.drawable.kfcmenu4,R.drawable.kfcmenu5)
+            map["menu3"] = arrayListOf(R.drawable.bkmenu0,R.drawable.bkmenu1,R.drawable.bkmenu2,R.drawable.bkmenu3,
+                R.drawable.bkmenu4,R.drawable.bkmenu5)
+            map["menu4"] = arrayListOf(R.drawable.birmenu0,R.drawable.birmenu0,R.drawable.birmenu2,R.drawable.birmenu3,
+                R.drawable.birmenu4,R.drawable.birmenu5)
+            map["menu5"] = arrayListOf(R.drawable.hotmenu0,R.drawable.hotmenu1,R.drawable.hotmenu2,R.drawable.hotmenu3,
+                R.drawable.hotmenu4,R.drawable.hotmenu5)
+
+            var imgArray = map["menu$num"]
+            while(i<6){
+                cardMaker(imgArray?.get(i) ?: R.drawable.timer_pic,menuArray[i]) { itemCount++ }
+                i++
+            }
+            Row(horizontalArrangement = Arrangement.SpaceAround, verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(top = 15.dp, bottom = 15.dp)) {
+                Divider(color = Color(0xFFD5D5D5), thickness = 1.dp, modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 10.dp))
+                Text(text = "  MORE COMING SOON  ", color = Color(0xFF8F8F8F), style = TextStyle(fontSize = 18.sp))
+                Divider(color = Color(0xFFD5D5D5), thickness = 1.dp, modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 10.dp))
+            }
         }
+
     }
 }
 
 @Composable
-fun cardMaker(menuNum : Int, itemCounter: () -> Unit ){
-    val menuId = R.array.Menu0 + menuNum
-    val menuArray = stringArrayResource(id = menuId)
-    for(menuItem in menuArray){
-        Card(modifier = Modifier
-            .fillMaxWidth(0.95f)
-            .wrapContentHeight()
-            .padding(top = 12.dp,),
-            elevation = CardDefaults.cardElevation(10.dp),
-            colors = CardDefaults.cardColors(Color.White)
-        ) {
-            Column(modifier = Modifier.fillMaxSize().padding(top = 8.dp, bottom = 8.dp),
-                verticalArrangement = Arrangement.Center) {
-                Row (modifier = Modifier){
-                    Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center) {
-                        Text(text = menuItem, style = TextStyle(fontFamily = FontFamily(Font((R.font.roboto_regular),
+fun cardMaker(imageId: Int, itemName: String, itemCounter: () -> Unit ){
+    Card(modifier = Modifier
+        .fillMaxWidth(0.95f)
+        .wrapContentHeight()
+        .padding(top = 15.dp,),
+        elevation = CardDefaults.cardElevation(10.dp),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        Column(modifier = Modifier
+            .fillMaxSize()) {
+            Row (modifier = Modifier){
+                Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxHeight()) {
+                    Image(painter = painterResource(id = imageId), contentDescription = "Food pic",
+                        modifier = Modifier.size(96.dp), contentScale = ContentScale.Crop)
+                    Column(modifier = Modifier.fillMaxHeight().fillMaxWidth(0.60f),
+                        verticalArrangement = Arrangement.Center) {
+                        Text(text = itemName, style = TextStyle(fontFamily = FontFamily(Font((R.font.roboto_regular),
                             weight = FontWeight(1))), fontSize = 22.sp),
-                            modifier = Modifier.padding(start = 10.dp))
+                            modifier = Modifier.padding(start = 10.dp), maxLines = 2, overflow = TextOverflow.Ellipsis)
                         Text(text = "₹ "+ makePrice().toString(), style = TextStyle(fontSize = 18.sp),
                             modifier = Modifier.padding(start = 10.dp, top = 8.dp))
                     }
-                    Box(modifier = Modifier.fillMaxSize()
-                        .align(Alignment.CenterVertically),) {
-                        Button(onClick = { itemCounter() }, modifier = Modifier
-                            .size(110.dp, 38.dp)
-                            .padding(end = 10.dp)
-                            .align(Alignment.CenterEnd)
-                            , colors = ButtonDefaults.buttonColors(Color(0xFFFFB3B9)),
-                            contentPadding = PaddingValues(),
-                            shape = RoundedCornerShape(8.dp),
-                            border = BorderStroke(1.dp, Color(0xFFE93545))
-                        ) {
-                            Text(text = "Add to Cart", color = Color(0xFFE93545), style = TextStyle(fontSize = 17.sp,
-                                fontWeight = FontWeight.Bold))
-                        }
+                }
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.CenterVertically),) {
+                    Button(onClick = { itemCounter() }, modifier = Modifier
+                        .size(110.dp, 38.dp)
+                        .padding(end = 10.dp)
+                        .align(Alignment.CenterEnd)
+                        , colors = ButtonDefaults.buttonColors(Color(0xFFFFB3B9)),
+                        contentPadding = PaddingValues(),
+                        shape = RoundedCornerShape(8.dp),
+                        border = BorderStroke(1.dp, Color(0xFFE93545))
+                    ) {
+                        Text(text = "Add to Cart", color = Color(0xFFE93545), style = TextStyle(fontSize = 17.sp,
+                            fontWeight = FontWeight.Bold), modifier = Modifier.padding(start = 2.dp, end = 2.dp))
                     }
                 }
             }
